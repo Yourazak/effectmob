7373a462a5a170abca85e04549b14521fecab41d
Albina Mollander <albinamo@student.21-school.ru>
2025-07-17 14:13:07 +0300

    конфиг

--- config/config.go
+++ config/config.go
@@ -1,9 +1,38 @@
 package config
 
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
 type Config struct {
-	DBhost     string
-	DBport     string
+	DBHost     string
+	DBPort     string
 	DBUser     string
 	DBPassword string
 	DBName     string
+}
+
+func LoadConfig() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: .env file not found, using environment variables")
+	}
+
+	return &Config{
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", ""),
+		DBName:     getEnv("DB_NAME", "sub"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return fallback
+	}
+	return value
 }
