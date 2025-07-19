b0b3da82b50e70ac2fe8d05af4e2a5ab661f3427
Albina Mollander <albinamo@student.21-school.ru>
2025-07-19 20:19:26 +0300

    final

--- cmd/main.go
+++ cmd/main.go
@@ -2,23 +2,29 @@ package main
 
 import (
 	"fmt"
-	_ "github.com/jackc/pgx/v5/stdlib"
-	"github.com/jmoiron/sqlx"
 	"log"
 	"net/http"
 	"service/config"
 	"service/internal/subscriptions"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+	httpSwagger "github.com/swaggo/http-swagger"
+	_ "service/docs" // импорт сгенерированных swagger-документов (путь подкорректируй!)
 )
 
+// @title Subscription Service API
+// @version 1.0
+// @description Сервис для управления онлайн-подписками
+// @host localhost:8080
+// @BasePath /
+// @schemes http
 func main() {
-	// Загружаем конфиг из .env
 	cfg := config.LoadConfig()
 
-	// Формируем строку подключения к базе
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
 
-	// Подключаемся к базе
 	db, err := sqlx.Connect("pgx", dsn)
 	if err != nil {
 		log.Fatalf("Ошибка подключения к базе данных: %v", err)
@@ -26,16 +32,16 @@ func main() {
 	defer db.Close()
 	log.Println("Успешное подключение к базе данных!")
 
-	// Создаем репозиторий, сервис и хендлер
 	repo := subscriptions.NewRepository(db)
 	service := subscriptions.NewService(repo)
 	handler := subscriptions.NewHandler(service)
 
-	// Регистрируем HTTP-ручки
+	// API endpoint для подписок
 	http.HandleFunc("/subscriptions", handler.SubscriptionsHandler)
-	// для CRUDL
 
-	// Запускаем HTTP сервер
+	// Swagger UI endpoint по адресу http://localhost:8080/swagger/index.html
+	http.Handle("/swagger/", httpSwagger.WrapHandler)
+
 	log.Println("Сервер запущен на :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("Ошибка запуска сервера: %v", err)
